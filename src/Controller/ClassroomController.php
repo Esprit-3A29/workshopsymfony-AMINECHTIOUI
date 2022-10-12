38fdf85d668bed0c6af3c73177edaaf078f507ee
AMINECHTIOUI <79096147+AMINECHTIOUI@users.noreply.github.com>
2022-10-12 21:55:45 +0100

    test

--- src/Controller/ClassroomController.php
+++ src/Controller/ClassroomController.php
@@ -24,25 +24,25 @@ class ClassroomController extends AbstractController
     public function listclass(ClassroomRepository $repository){
         $class=$repository->findAll();
 
-        return $this->render("classroom/listclass.html.twig",array("tabclass"=>$class));
+        return $this->render("classroom/listclass.html.twig",array("tab_class"=>$class));
     }
     #[Route('/add_class', name: 'add_class')]
     public function addform(ManagerRegistry $doctrine ,Request $request){
        
-        $class= new Classroom;
+        $class= new Classroom();
 $form = $this->createForm(ClassroomType::class,$class);
 $form->handleRequest($request);
 if($form->isSubmitted()){
 $em = $doctrine->getManager();
 $em->persist($class);
 $em->flush();
-return $this->redirectToRoute ("tabclass");
+return $this->redirectToRoute ("tab_class");
 
 }
-        return $this->renderForm("classroom/add.html.twig",array("formClassroom"=>$class));
+        return $this->renderForm("classroom/add.html.twig",array("formClassroom"=>$form));
     }
-    #[Route('/update_class', name: 'update_class')]
-    public function updateform(ManagerRegistry $doctrine ,Request $request){
+    #[Route('/update_class/{id}', name: 'update_class')]
+    public function updateform($id ,ClassroomRepository $repository ,ManagerRegistry $doctrine ,Request $request){
        
         $class= $repository->find($id);
 $form = $this->createForm(ClassroomType::class,$class);
@@ -50,10 +50,10 @@ $form->handleRequest($request);
 if($form->isSubmitted()){
 $em = $doctrine->getManager();
 $em->flush();
-return $this->redirectToRoute ("tabclass");
+return $this->redirectToRoute("tab_class");
 
 }
-        return $this->renderForm("classroom/update.html.twig",array("formClassroom"=>$class));
+        return $this->renderForm("classroom/update.html.twig",array("formClassroom"=>$form));
     }
     #[Route('/removeform/{id}', name: 'remove')]
     public function remove(ManagerRegistry $doctrine,$id , ClassroomRepository $repository  ){
@@ -62,6 +62,6 @@ return $this->redirectToRoute ("tabclass");
        $em = $doctrine->getManager();
        $em->remove($class);
        $em->flush() ; 
-        return $this->redirectToRoute("tabclass");
+        return $this->redirectToRoute("tab_class");
     }
    }
--- src/Form/ClassroomType.php
+++ src/Form/ClassroomType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Classroom;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -14,7 +15,8 @@ class ClassroomType extends AbstractType
         $builder
             ->add('name')
             ->add('description')
-        ;
+            ->add('Submit',SubmitType::class)
+            ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
